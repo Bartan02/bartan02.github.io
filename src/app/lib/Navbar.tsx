2e37b69ea56f5fe2e6426c157470b0f32d19a542
Bartosz Adamczyk <adam0016@hz.nl>
2024-12-16 00:31:48 +0100

    Configure internationalization. Add Polish translation.

--- src/app/lib/Navbar.tsx
+++ src/app/lib/Navbar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 import darkModeOff from "@/public/image/icons/icons8-dark-mode-32.png";
 import darkModeOn from "@/public/image/icons/icons8-do-not-disturb-ios-32.png";
+import {useLocale, useTranslations} from "next-intl";
+import { useRouter } from 'next/navigation'
 
 export default function Navbar() {
     const [toggled, setToggle] = useState(false);
@@ -36,18 +38,25 @@ export default function Navbar() {
         document.documentElement.classList.toggle("dark-mode", isDarkMode);
     }, [isDarkMode]);
 
+    const t = useTranslations("");
+
+    const locale = useLocale();
+
+    const router = useRouter()
+
+
     return (
         <nav className="fixed m-8 text-xl right-0 md:right-5 z-20">
-            <div className="navlist list-none flex gap-4">
+            <div className="navlist list-none flex gap-4 items-center">
                 <div className={'md:flex hidden gap-4 transition'}>
                     <span className="hover:font-[900]">
-                        <Link href="#about-me">About me</Link>
+                        <Link href="#about-me">{t('About me')}</Link>
                     </span>
                     <span className="hover:font-[900]">
-                        <Link href="#portfolio">Portfolio</Link>
+                        <Link href="#portfolio">{t('Portfolio')}</Link>
                     </span>
                     <span className="hover:font-[900]">
-                        <Link href="#contact-me">Contact</Link>
+                        <Link href="#contact-me">{t('Contact')}</Link>
                     </span>
                 </div>
                 <div className={'md:hidden flex gap-4'}>
@@ -72,17 +81,27 @@ export default function Navbar() {
                         height={24}
                     />
                 </span>
+                <div className={'gap-4 cursor-pointer '}>
+                    <select name="lang" id="lang" className={'bg-transparent border-0'} value={locale} onChange={async e => {
+                        const currentPath = window.location.pathname.slice(3)
+                        const targetPath = '/' + e.target.value + currentPath
+                        router.push(targetPath)
+                    }}>
+                        <option value="en">🇬🇧</option>
+                        <option value="pl">🇵🇱</option>
+                    </select>
+                </div>
             </div>
             <div className={`mobile-nav ${toggled ? "fixed" : "hidden"} w-full h-dvh top-0 left-0 -z-10 opacity-100 bg-[--background] content-center text-center text-6xl`}>
                 <ul>
                     <li className="hover:font-[900] py-5">
-                        <Link href="#about-me" onClick={() => setToggle(!toggled)}>About me</Link>
+                        <Link href="#about-me" onClick={() => setToggle(!toggled)}>{t('About me')}</Link>
                     </li>
                     <li className="hover:font-[900] py-5">
-                        <Link href="#portfolio" onClick={() => setToggle(!toggled)}>Portfolio</Link>
+                        <Link href="#portfolio" onClick={() => setToggle(!toggled)}>{t('Portfolio')}</Link>
                     </li>
                     <li className="hover:font-[900] py-5">
-                        <Link href="#contact-me" onClick={() => setToggle(!toggled)}>Contact</Link>
+                        <Link href="#contact-me" onClick={() => setToggle(!toggled)}>{t('Contact')}</Link>
                     </li>
                 </ul>
             </div>
