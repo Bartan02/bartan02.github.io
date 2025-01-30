d88ee8e370d6310c17d5d4de0120a72f11c67308
Bartosz Adamczyk <adam0016@hz.nl>
2025-01-30 15:19:05 +0100

    Improve language switcher in Navbar

--- src/app/lib/Navbar.tsx
+++ src/app/lib/Navbar.tsx
@@ -1,11 +1,15 @@
 "use client"
-import { useState, useEffect } from "react";
+import {useState, useEffect, useLayoutEffect} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import darkModeOff from "@/public/image/icons/icons8-dark-mode-32.png";
 import darkModeOn from "@/public/image/icons/icons8-do-not-disturb-ios-32.png";
 import { useLocale, useTranslations } from "next-intl";
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
+import uk_icon from '@/public/image/icons/header/flags/united-kingdom.png';
+import nl_icon from '@/public/image/icons/header/flags/netherlands.png';
+import fr_icon from '@/public/image/icons/header/flags/france.png';
+import pl_icon from '@/public/image/icons/header/flags/poland.png';
 
 export default function Navbar() {
     const [toggled, setToggle] = useState(false);
@@ -59,31 +63,74 @@ export default function Navbar() {
         window.addEventListener('scroll', onScroll)
     }, []);
 
+    // Language switcher
+
+    const [isLanguageSwitch, setIsLanguageSwitch] = useState(false);
+
+    function changeLanguage(targetLang: string) {
+        setIsLanguageSwitch(!isLanguageSwitch)
+        if(locale !== targetLang){
+            const currentPath = window.location.pathname.slice(3);
+            const targetPath = '/' + targetLang + currentPath;
+            router.push(targetPath);
+        }
+        return undefined;
+    }
+
+    const [imageSrc, setImageSrc] = useState<string>(uk_icon);
+
+    useLayoutEffect(() => {
+        switch(locale){
+            case 'en':
+                setImageSrc(uk_icon);
+                break;
+            case 'fr':
+                setImageSrc(fr_icon);
+                break;
+            case 'nl':
+                setImageSrc(nl_icon);
+                break;
+            case 'pl':
+                setImageSrc(pl_icon);
+                break;
+        }
+    }, []);
+
     return (
-        <nav id="nav" className="transition-all fixed m-8 text-xl right-0 md:right-5 z-20 bg-[--landing-screen] rounded-full p-2 px-3 sm:px-4">
+        <nav id="nav" className="fixed m-8 text-xl right-0 md:right-5 z-20 bg-[--landing-screen] rounded-full p-2 px-3 sm:px-4">
             <div className="navlist list-none flex gap-4 items-center">
                 <div className={'md:flex hidden gap-4'}>
-                    <span className="hover:font-[900]">
+                    <span>
                         <Link href="#about-me">{t('About me')}</Link>
                     </span>
-                    <span className="hover:font-[900]">
+                    <span>
                         <Link href="#portfolio">{t('Portfolio')}</Link>
                     </span>
-                    <span className="hover:font-[900]">
+                    <span>
                         <Link href="#contact-me">{t('Contact')}</Link>
                     </span>
                 </div>
-                <div className={'gap-4 cursor-pointer'}>
-                    <select name="lang" id="lang" className={'bg-transparent border-0'} value={locale} onChange={async e => {
-                        const currentPath = window.location.pathname.slice(3)
-                        const targetPath = '/' + e.target.value + currentPath
-                        router.push(targetPath)
-                    }}>
-                        <option value="en">EN</option>
-                        <option value="fr">FR</option>
-                        <option value="nl">NL</option>
-                        <option value="pl">PL</option>
-                    </select>
+                <div className={'cursor-pointer transition-all'}>
+                    <Image src={imageSrc} alt={'Flag'} onClick={() => {setIsLanguageSwitch(!isLanguageSwitch)}}/>
+                    <div className={`z-0 gap-4 text-sm absolute bg-[--landing-screen] p-3 px-6 rounded-full shadow-2xl md:left-[65px] -left-40 md:top-16 top-12 ${isLanguageSwitch ? "flex motion-preset-slide-down motion-duration-200" : 'hidden'}`}>
+                        <div className={'flex flex-col items-center gap-1 hover:bg-[--highlight] rounded'} onClick={() => {changeLanguage('en')}}>
+                            <Image src={uk_icon} alt={'UK flag'}/>
+                            english
+                        </div>
+                        <div className={'flex flex-col items-center gap-1 hover:bg-[--highlight] rounded'} onClick={() => {changeLanguage('fr')}}>
+                            <Image src={fr_icon} alt={'FR flag'}/>
+                            français
+                        </div>
+                        <div className={'flex flex-col items-center gap-1 hover:bg-[--highlight] rounded'} onClick={() => {changeLanguage('nl')}}>
+                            <Image src={nl_icon} alt={'NL flag'}/>
+                            nederlands
+                        </div>
+                        <div className={'flex flex-col items-center gap-1 hover:bg-[--highlight] rounded'} onClick={() => {changeLanguage('pl')}}>
+                            <Image src={pl_icon} alt={'PL flag'}/>
+                            polski
+                        </div>
+                    </div>
+                    <div className={`-z-10 absolute border-x-[20px] border-x-transparent border-b-[25px] border-b-[--landing-screen] md:top-12 md:right-12 right-[84px] top-11 ${isLanguageSwitch ? "block motion-preset-slide-down motion-duration-200" : 'hidden'}`}></div>
                 </div>
                 <span
                     className="hover:font-[900] hover:cursor-pointer"
@@ -96,8 +143,8 @@ export default function Navbar() {
                         height={24}
                     />
                 </span>
-                <div className={'md:hidden flex gap-4'}>
-                    <div className="inline-block cursor-pointer w-[24px] h-[24px] xl:hidden content-center" onClick={() => setToggle(!toggled)}>
+                <div className={'md:hidden flex'}>
+                    <div className="inline-block cursor-pointer w-[24px] h-[24px] xl:hidden content-center" onClick={() => {setToggle(!toggled); setIsLanguageSwitch(false)}}>
                         <div
                             className={`${toggled ? "change" : ""}`}
                         >
@@ -111,13 +158,13 @@ export default function Navbar() {
             <div className={`mobile-nav ${toggled ? "fixed" : "hidden"} w-full h-dvh top-0 left-0 -z-10 opacity-100 bg-[--background] content-center text-center text-6xl`}>
                 <ul>
                     <li className="hover:font-[900] py-5">
-                        <Link href="#about-me" onClick={() => setToggle(!toggled)}>{t('About me')}</Link>
+                        <Link href="#about-me" onClick={() => {setToggle(!toggled); setIsLanguageSwitch(false)}}>{t('About me')}</Link>
                     </li>
                     <li className="hover:font-[900] py-5">
-                        <Link href="#portfolio" onClick={() => setToggle(!toggled)}>{t('Portfolio')}</Link>
+                        <Link href="#portfolio" onClick={() => {setToggle(!toggled); setIsLanguageSwitch(false)}}>{t('Portfolio')}</Link>
                     </li>
                     <li className="hover:font-[900] py-5">
-                        <Link href="#contact-me" onClick={() => setToggle(!toggled)}>{t('Contact')}</Link>
+                        <Link href="#contact-me" onClick={() => {setToggle(!toggled); setIsLanguageSwitch(false)}}>{t('Contact')}</Link>
                     </li>
                 </ul>
             </div>
